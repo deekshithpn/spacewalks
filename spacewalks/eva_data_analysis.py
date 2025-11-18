69aa6d6d68167cab57d25c0844285c784c82f8f2
Aleksandra Nenadic <a.nenadic@manchester.ac.uk>
2025-11-18 15:45:56 +0000

    Update eva_data_analysis.py

--- spacewalks/eva_data_analysis.py
+++ spacewalks/eva_data_analysis.py
@@ -27,14 +27,14 @@ def read_json_to_dataframe(input_file):
     Clean the data by removing any incomplete rows and sort by date
 
     Args:
-        input_file (str): The path to the JSON file.
+        input_file (file or str): The file object or path to the JSON file.
 
     Returns:
          eva_df (pd.DataFrame): The cleaned and sorted data as a dataframe structure
     """
     print(f'Reading JSON file {input_file}')
     # Read the data from a JSON file into a Pandas dataframe
-    eva_df = pd.read_json(input_file, convert_dates=['date'])
+    eva_df = pd.read_json(input_file, convert_dates=['date'], encoding='ascii')
     eva_df['eva'] = eva_df['eva'].astype(float)
     # Clean the data by removing any incomplete rows
     eva_df.dropna(axis=0, inplace=True)
@@ -47,14 +47,14 @@ def write_dataframe_to_csv(df, output_file):
 
     Args:
         df (pd.DataFrame): The input dataframe.
-        output_file (str): The path to the output CSV file.
+        output_file (file or str): The file object or path to the output CSV file.
 
     Returns:
         None
     """
     print(f'Saving to CSV file {output_file}')
     # Save dataframe to CSV file for later analysis
-    df.to_csv(output_file, index=False)
+    df.to_csv(output_file, index=False, encoding='utf-8')
 
 def plot_cumulative_time_in_space(df, graph_file):
     """
@@ -67,13 +67,13 @@ def plot_cumulative_time_in_space(df, graph_file):
 
     Args:
         df (pd.DataFrame): The input dataframe.
-        graph_file (str): The path to the output graph file.
+        graph_file (file or str): The file object or path to the output graph file.
 
     Returns:
         None
     """
     print(f'Plotting cumulative spacewalk duration and saving to {graph_file}')
-    df = add_duration_hours_variable(df)
+    df['duration_hours'] = df['duration'].str.split(":").apply(lambda x: int(x[0]) + int(x[1])/60)
     df['cumulative_time'] = df['duration_hours'].cumsum()
     plt.plot(df['date'], df['cumulative_time'], 'ko-')
     plt.xlabel('Year')
