6f87dee0465ad708ee89bf2c70357e44207f31ac
Deekshith <deekshithnaidus@gmail.com>
2026-07-22 10:47:41 -0500

    added a test code and debugged the code

---
+++ spacewalks/test_code.py
@@ -0,0 +1,11 @@
+import pytest
+from eva_data_analysis import text_to_duration
+
+def test_text_to_duration_integer():
+    assert text_to_duration("10:00") == 10
+
+def test_text_to_duration_float():
+    assert text_to_duration("10:15") == 10.25
+
+def test_text_to_duration_rational():
+    assert text_to_duration("10:20") == pytest.approx(10.333333)
